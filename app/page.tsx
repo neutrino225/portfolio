e426945ec7b76c8076959dbfa1f3c06bdcf113d8
neutrino225 <fznkrmt22503@gmail.com>
2024-07-19 19:56:52 +0500

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,159 @@
+/** @format */
+
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<main className="flex min-h-screen flex-col items-start justify-start p-24 gap-36 overflow-hidden">
+			<section
+				id="home"
+				className="mx-auto w-2/3 h-full flex flex-col gap-10 mt-16">
+				<div className="w-min flex flex-col justify-start items-start gap-5">
+					{/* Color Bars */}
+					<div className="w-full flex gap-1">
+						<span className="w-24 h-4 rounded-3xl bg-blue-300/70"></span>
+						<span className="w-80 h-4 rounded-3xl bg-red-300/70"></span>
+						<span className="w-20 h-4 rounded-3xl bg-green-300/70"></span>
+						<span className="w-5 h-4 rounded-3xl bg-purple-300/70"></span>
+					</div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+					<div className="w-full flex gap-1">
+						<span className="w-5 h-4 rounded-full bg-transparent"></span>
+						<span className="w-32 h-4 rounded-full bg-yellow-300/70"></span>
+						<span className="w-14 h-4 rounded-full bg-blue-300/70"></span>
+						<span className="w-28 h-4 rounded-full bg-teal-300/70"></span>
+					</div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+					<div className="w-full flex gap-1">
+						<span className="w-20 h-4 rounded-full bg-green-300/70"></span>
+						<span className="w-24 h-4 rounded-full bg-red-300/70"></span>
+						<span className="w-16 h-4 rounded-full bg-blue-300/70"></span>
+						<span className="w-20 h-4 rounded-full bg-purple-300/70"></span>
+					</div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+					<div className="w-full flex gap-1">
+						<span className="w-10 h-4 rounded-full bg-transparent"></span>
+						<span className="w-96 h-4 rounded-full bg-red-300/70"></span>
+						<span className="w-10 h-4 rounded-full bg-purple-300/70"></span>
+					</div>
+				</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+				<div className="w-full flex flex-col justify-start items-start ml-10">
+					<h1 className="text-5xl text-slate-200 font-bold">Faizan Karamat</h1>
+					<h2 className="text-2xl text-slate-200 font-light">
+						FullStack Developer
+					</h2>
+				</div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+				{/* Color Bars */}
+				<div className="w-min flex flex-col justify-start items-start gap-5">
+					<div className="w-full flex gap-1">
+						<span className="w-24 h-4 rounded-3xl bg-blue-300/70"></span>
+						<span className="w-80 h-4 rounded-3xl bg-red-300/70"></span>
+						<span className="w-20 h-4 rounded-3xl bg-green-300/70"></span>
+						<span className="w-5 h-4 rounded-3xl bg-purple-300/70"></span>
+					</div>
+
+					<div className="w-full flex gap-1">
+						<span className="w-10 h-4 rounded-full bg-transparent"></span>
+						<span className="w-96 h-4 rounded-full bg-red-300/70"></span>
+						<span className="w-10 h-4 rounded-full bg-purple-300/70"></span>
+					</div>
+
+					<div className="w-full flex gap-1">
+						<span className="w-5 h-4 rounded-full bg-transparent"></span>
+						<span className="w-32 h-4 rounded-full bg-yellow-300/70"></span>
+						<span className="w-14 h-4 rounded-full bg-blue-300/70"></span>
+						<span className="w-28 h-4 rounded-full bg-teal-300/70"></span>
+					</div>
+
+					<div className="w-full flex gap-1">
+						<span className="w-20 h-4 rounded-full bg-green-300/70"></span>
+						<span className="w-24 h-4 rounded-full bg-red-300/70"></span>
+						<span className="w-16 h-4 rounded-full bg-blue-300/70"></span>
+						<span className="w-20 h-4 rounded-full bg-purple-300/70"></span>
+					</div>
+				</div>
+			</section>
+
+			{/* about section */}
+			<section
+				id="aboutme"
+				className="mx-auto w-2/3 flex flex-col justify-start items-start gap-5">
+				<h1 className="text-lg text-blue-400 font-bold flex justify-center items-center gap-2">
+					About Me <span className="w-4 h-1 bg-blue-400 rounded-2xl"></span>
+				</h1>
+
+				<div className="w-full flex flex-col justify-center items-start gap-3">
+					<h1 className="text-2xl text-slate-100 font-semibold">
+						Hi I'm Faizan Karamat
+					</h1>
+					<p className="text-pretty text-lg font-light text-slate-200">
+						I'm a self-taught Full Stack Developer with a focus on the MERN
+						stack (that's fancy talk for MongoDB, Express, React, and Node.js).
+						I've been at it for over 2 years now, and I'm especially pumped
+						about building user interfaces with tools like Next.js and Svelte.
+						My code isn't just functional, it's fun to use! Beyond web
+						development, I'm diving into the world of AI – machine learning and
+						deep learning specifically. I'm passionate about using technology to
+						make things not just new, but better, more efficient, and way more
+						user-friendly.
+					</p>
+				</div>
+			</section>
+
+			{/* skills section */}
+			<section
+				id="skills"
+				className="mx-auto w-2/3 flex flex-col justify-start items-start gap-3">
+				<h1 className="text-lg text-blue-400 font-bold flex justify-center items-center gap-2">
+					Skills <span className="w-4 h-1 bg-blue-400 rounded-2xl"></span>
+				</h1>
+
+				<div className="w-full flex flex-col justify-center items-start gap-7">
+					<p className="text-pretty text-lg font-light text-slate-200">
+						Here are some of the technologies I've been working with:
+					</p>
+
+					<div className="w-full grid grid-cols-3 gap-5">
+						<div className="w-full flex flex-col justify-start items-start gap-3 bg-card shadow-2xl p-5 rounded-2xl">
+							<h1 className="text-xl text-slate-100 font-semibold">Frontend</h1>
+							<ul className="text-pretty text-lg font-light text-slate-200 list-disc list-inside">
+								<li>React</li>
+								<li>Next.js</li>
+								<li>Svelte</li>
+								<li>Tailwind CSS</li>
+								<li>SCSS</li>
+								<li>CSS Modules</li>
+								<li>CSS-in-JS</li>
+							</ul>
+						</div>
+
+						<div className="w-full flex flex-col justify-start items-start gap-3 bg-card shadow-2xl p-5 rounded-2xl">
+							<h1 className="text-xl text-slate-100 font-semibold">Backend</h1>
+							<ul className="text-pretty text-lg font-light text-slate-200 list-disc list-inside">
+								<li>Node.js</li>
+								<li>Express</li>
+								<li>MongoDB</li>
+								<li>Mongoose</li>
+								<li>REST APIs</li>
+							</ul>
+						</div>
+
+						<div className="w-full flex flex-col justify-start items-start gap-3 bg-card shadow-2xl p-5 rounded-2xl">
+							<h1 className="text-xl text-slate-100 font-semibold">AI</h1>
+							<ul className="text-pretty text-lg font-light text-slate-200 list-disc list-inside">
+								<li>Python</li>
+								<li>TensorFlow</li>
+								<li>Keras</li>
+								<li>PyTorch</li>
+								<li>Scikit-learn</li>
+								<li>OpenCV</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+	);
 }
