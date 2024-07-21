33e56ddb0239ec5754de9cefec41644f69466516
neutrino225 <fznkrmt22503@gmail.com>
2024-07-21 23:14:47 +0500

    1.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,39 @@
 /** @format */
 
-import Image from "next/image";
+const skills = [
+	"HTML",
+	"CSS",
+	"JavaScript",
+	"TypeScript",
+	"React",
+	"Next.js",
+	"Svelte",
+	"Node.js",
+	"Express",
+	"MongoDB",
+	"Firebase",
+	"TailwindCSS",
+	"Bootstrap",
+	"Material-UI",
+	"Chakra UI",
+	"REST API",
+	"PostgreSQL",
+	"MySQL",
+	"Python",
+	"TensorFlow",
+	"Keras",
+	"PyTorch",
+	"Machine Learning",
+	"Deep Learning",
+	"Flask",
+];
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-start justify-start p-24 gap-36 overflow-hidden">
+		<main className="flex min-h-screen flex-col items-start justify-start p-10 md:p-24 gap-36 overflow-hidden">
 			<section
 				id="home"
-				className="mx-auto w-2/3 h-full flex flex-col gap-10 mt-16">
+				className="mx-auto w-full md:w-2/3 h-full flex flex-col gap-10 mt-16">
 				<div className="w-min flex flex-col justify-start items-start gap-5">
 					{/* Color Bars */}
 					<div className="w-full flex gap-1">
@@ -49,9 +75,9 @@ export default function Home() {
 				<div className="w-min flex flex-col justify-start items-start gap-5">
 					<div className="w-full flex gap-1">
 						<span className="w-24 h-4 rounded-3xl bg-blue-300/70"></span>
+						<span className="w-5 h-4 rounded-3xl bg-purple-300/70"></span>
 						<span className="w-80 h-4 rounded-3xl bg-red-300/70"></span>
 						<span className="w-20 h-4 rounded-3xl bg-green-300/70"></span>
-						<span className="w-5 h-4 rounded-3xl bg-purple-300/70"></span>
 					</div>
 
 					<div className="w-full flex gap-1">
@@ -102,57 +128,44 @@ export default function Home() {
 				</div>
 			</section>
 
+			{/* projects section */}
+			<section
+				id="projects"
+				className="mx-auto w-2/3 flex flex-col justify-start items-start gap-3">
+				<h1 className="text-lg text-blue-400 font-bold flex justify-center items-center gap-2">
+					Projects <span className="w-4 h-1 bg-blue-400 rounded-2xl"></span>
+				</h1>
+
+				<div className="w-full grid grid-cols-4 gap-5"></div>
+			</section>
+
 			{/* skills section */}
 			<section
 				id="skills"
-				className="mx-auto w-2/3 flex flex-col justify-start items-start gap-3">
+				className="mx-auto w-2/3 flex flex-col justify-start items-start gap-5">
 				<h1 className="text-lg text-blue-400 font-bold flex justify-center items-center gap-2">
 					Skills <span className="w-4 h-1 bg-blue-400 rounded-2xl"></span>
 				</h1>
 
-				<div className="w-full flex flex-col justify-center items-start gap-7">
-					<p className="text-pretty text-lg font-light text-slate-200">
-						Here are some of the technologies I've been working with:
-					</p>
-
-					<div className="w-full grid grid-cols-3 gap-5">
-						<div className="w-full flex flex-col justify-start items-start gap-3 bg-card shadow-2xl p-5 rounded-2xl">
-							<h1 className="text-xl text-slate-100 font-semibold">Frontend</h1>
-							<ul className="text-pretty text-lg font-light text-slate-200 list-disc list-inside">
-								<li>React</li>
-								<li>Next.js</li>
-								<li>Svelte</li>
-								<li>Tailwind CSS</li>
-								<li>SCSS</li>
-								<li>CSS Modules</li>
-								<li>CSS-in-JS</li>
-							</ul>
+				{/* // map skills onto chips  */}
+				<div className="w-full flex flex-wrap justify-start items-start gap-4">
+					{skills.map((skill, index) => (
+						<div
+							key={index}
+							className="px-3 py-2 bg-[#111315] text-slate-100 border border-blue-950 select-none">
+							{skill}
 						</div>
-
-						<div className="w-full flex flex-col justify-start items-start gap-3 bg-card shadow-2xl p-5 rounded-2xl">
-							<h1 className="text-xl text-slate-100 font-semibold">Backend</h1>
-							<ul className="text-pretty text-lg font-light text-slate-200 list-disc list-inside">
-								<li>Node.js</li>
-								<li>Express</li>
-								<li>MongoDB</li>
-								<li>Mongoose</li>
-								<li>REST APIs</li>
-							</ul>
-						</div>
-
-						<div className="w-full flex flex-col justify-start items-start gap-3 bg-card shadow-2xl p-5 rounded-2xl">
-							<h1 className="text-xl text-slate-100 font-semibold">AI</h1>
-							<ul className="text-pretty text-lg font-light text-slate-200 list-disc list-inside">
-								<li>Python</li>
-								<li>TensorFlow</li>
-								<li>Keras</li>
-								<li>PyTorch</li>
-								<li>Scikit-learn</li>
-								<li>OpenCV</li>
-							</ul>
-						</div>
-					</div>
+					))}
 				</div>
+			</section>
+
+			{/* contact section */}
+			<section
+				id="contact"
+				className="mx-auto w-2/3 flex flex-col justify-start items-start gap-3">
+				<h1 className="text-lg text-blue-400 font-bold flex justify-center items-center gap-2">
+					Contact <span className="w-4 h-1 bg-blue-400 rounded-2xl"></span>
+				</h1>
 			</section>
 		</main>
 	);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config: Config = {
 			backgroundColor: {
 				card: "#111315",
 			},
+			boxShadow: {
+				card: "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;",
+			},
 		},
 	},
 	plugins: [],
